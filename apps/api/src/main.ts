76d4113ecf661b92b3f53b57024821a1ff63c7a0
Cristian <cbperaltasegura@gmail.com>
2026-06-14 11:50:21 -0500

    fix(api): change default port to 3002

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   // Start the application and listen on port 3000
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3002);
 }
 
 // Bootstrap the application
